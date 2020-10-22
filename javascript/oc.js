99a0a64b1ec48f1170a97af4babf0e3f2f5374e6
irene hualde <irenehualde@hotmail.com>
2020-10-22 09:48:08 -0400

    Inclusion de arreglos para reducir codigo

--- javascript/oc.js
+++ javascript/oc.js
@@ -1,70 +1,40 @@
 
 
-function cambioimagen(opcion){
+function cambioimagen(i){
+    var imagen1=["imagenes/ciro/FOTO CIRO.png",'imagenes/gato/Bills f.jpg',"imagenes/granada/ciro-hernandez-2.jpg","imagenes/krilin/krilin1.jpg","imagenes/mono/mono1.jpg","imagenes/muñeca/ciro-hernandez-6.jpg","imagenes/panda/panda1.jpg"];
+    var imagen2=["imagenes/ciro/FOTO CIRO.png","imagenes/gato/Bills b.jpg","imagenes/granada/ciro-hernandez-1.jpg","imagenes/krilin/krilin2.jpg", "imagenes/mono/mono2.jpg","imagenes/muñeca/ciro-hernandez-2.jpg","imagenes/panda/panda2.jpg"];
+    var imagen3=["imagenes/ciro/FOTO CIRO.png","imagenes/gato/Bills l.jpg","imagenes/granada/ciro-hernandez-3.jpg","imagenes/krilin/krilin3.jpg","imagenes/mono/mono3.jpg","imagenes/muñeca/ciro-hernandez-3.jpg","imagenes/panda/panda3.jpg"];
+    var hslider1=['Ciro Hernandez','Gato','Granada','Krilin','mono','muñeca','panda'];
+    var tslider1=['Graphic Designer, 3D art','Presentacion frontal del gato','Presentacion frontal del granada','Presentacion frontal del krilin','Presentacion frontal del mono','Presentacion frontal del muñeca','Presentacion frontal del panda'];
+    var tslider2=['Graphic Designer, 3D art','Presentacion trasera del gato','Presentacion trasera del granada','Presentacion trasera del krilin','Presentacion trasera del mono','Presentacion trasera del muñeca','Presentacion trasera del panda'];
+    var tslider3=['Graphic Designer, 3D art','Presentacion lateral del gato','Presentacion lateral del granada','Presentacion lateral del krilin','Presentacion lateral del mono','Presentacion lateral del muñeca','Presentacion lateral del panda'];
 
-switch (opcion) {
+            // asignamos imagenes carrusel
+            document.getElementById("primero").src =imagen1[i];
+            document.getElementById("segundo").src =imagen2[i];
+            document.getElementById("tercero").src =imagen3[i];
+ 
+            // titulo de cada imagen del carrusel
+            document.getElementById("hslide").innerHTML = hslider1[i];
+            document.getElementById("hslide2").innerHTML = hslider1[i]; 
+            document.getElementById("hslide3").innerHTML = hslider1[i];
 
-case imagen0: document.getElementById("primero").src = "imagenes/ciro/FOTO CIRO.png";
-              document.getElementById("segundo").src = "imagenes/ciro/FOTO CIRO.png"; document.getElementById("tercero").src =
-              "imagenes/ciro/FOTO CIRO.png"; document.getElementById("hslide").innerHTML = 'Ciro Hernandez';
-              document.getElementById("hslide2").innerHTML = 'Ciro Hernandez'; document.getElementById("hslide3").innerHTML = 'Ciro Hernandez'; document.getElementById("tslide1").innerHTML = 'Graphic Designer, 3D art';
-              document.getElementById("tslide2").innerHTML = 'Maya, Mudbxo, ZBrush,Phptoshop,';
-              document.getElementById("tslide3").innerHTML = 'Metal Ray, FurryBall and others Software.';
-
-break;
-
-case imagen1: document.getElementById("primero").src = "imagenes/gato/Bills f.jpg";
-              document.getElementById("segundo").src = "imagenes/gato/Bills b.jpg"; document.getElementById("tercero").src =
-              "imagenes/gato/Bills l.jpg"; document.getElementById("hslide").innerHTML = 'Gato';
-              document.getElementById("hslide2").innerHTML = 'Gato'; document.getElementById("hslide3").innerHTML = 'Gato';
-              document.getElementById("tslide1").innerHTML = 'Presentacion frontal del gato';
-              document.getElementById("tslide2").innerHTML = 'Presentacion trasera del gato';
-              document.getElementById("tslide3").innerHTML = 'Presentacion lateral del gato';
-
-break;
-
-
-case imagen2: document.getElementById("primero").src = "imagenes/granada/ciro-hernandez-1.jpg"; document.getElementById("segundo").src
-              = "imagenes/granada/ciro-hernandez-2.jpg"; document.getElementById("tercero").src =
-              "imagenes/granada/ciro-hernandez-3.jpg"; document.getElementById("hslide").innerHTML = 'Granada';
-              document.getElementById("hslide2").innerHTML ='Granada'; document.getElementById("hslide3").innerHTML = 'Granada';
-              document.getElementById("tslide1").innerHTML = 'Presentacion frontal del granada';
-              document.getElementById("tslide2").innerHTML = 'Presentacion trasera del granada';
-              document.getElementById("tslide3").innerHTML = 'Presentacion lateral del granada';
-break;
-case imagen3: document.getElementById("primero").src = "imagenes/krilin/krilin1.jpg"; document.getElementById("segundo").src =
-              "imagenes/krilin/krilin2.jpg"; document.getElementById("tercero").src = "imagenes/krilin/krilin3.jpg";
-              document.getElementById("hslide").innerHTML = 'Krilin'; document.getElementById("hslide2").innerHTML ='Krilin';
-              document.getElementById("hslide3").innerHTML = 'Krilin'; document.getElementById("tslide1").innerHTML = 'Presentacion frontal del krilin'; document.getElementById("tslide2").innerHTML = 'Presentacion trasera del krilin';
-              document.getElementById("tslide3").innerHTML = 'Presentacion lateral del krilin';
-break;
-case imagen4: document.getElementById("primero").src = "imagenes/mono/mono1.jpg"; document.getElementById("segundo").src =
-              "imagenes/mono/mono2.jpg"; document.getElementById("tercero").src = "imagenes/mono/mono3.jpg";
-              document.getElementById("hslide").innerHTML = 'Mono'; document.getElementById("hslide2").innerHTML ='Mono';
-              document.getElementById("hslide3").innerHTML = 'Mono'; document.getElementById("tslide1").innerHTML = 'Presentacion frontal del mono'; document.getElementById("tslide2").innerHTML = 'Presentacion trasera del mono';
-              document.getElementById("tslide3").innerHTML = 'Presentacion lateral del mono';
-break; 
+            // descripcion de cada imagen del carrusel
+            document.getElementById("tslide1").innerHTML = tslider1[i];
+            document.getElementById("tslide2").innerHTML = tslider2[i];
+            document.getElementById("tslide3").innerHTML = tslider3[i];
+// switch (opcion) {
 
 
-case imagen5: document.getElementById("primero").src = "imagenes/muñeca/ciro-hernandez-6.jpg";
-              document.getElementById("segundo").src = "imagenes/muñeca/ciro-hernandez-2.jpg"; document.getElementById("tercero").src
-              = "imagenes/muñeca/ciro-hernandez-3.jpg"; document.getElementById("hslide").innerHTML = 'Muñeca';
-              document.getElementById("hslide2").innerHTML = 'Muñeca'; document.getElementById("hslide3").innerHTML = 'Muñeca';
-              document.getElementById("tslide1").innerHTML = 'Presentacion frontal del muñeca';
-              document.getElementById("tslide2").innerHTML = 'Presentacion trasera del muñeca';
-              document.getElementById("tslide3").innerHTML = 'Presentacion lateral del muñeca';
-break;
+// case imagen6: document.getElementById("primero").src = "imagenes/panda/panda1.jpg"; document.getElementById("segundo").src =
+//               "imagenes/panda/panda2.jpg"; document.getElementById("tercero").src = "imagenes/panda/panda3.jpg";
+//               document.getElementById("hslide").innerHTML = 'Panda'; document.getElementById("hslide2").innerHTML ='Panda';
+//               document.getElementById("hslide3").innerHTML = 'Panda'; document.getElementById("tslide1").innerHTML = 'Presentacion frontal del panda'; document.getElementById("tslide2").innerHTML = 'Presentacion trasera del panda';
+//               document.getElementById("tslide3").innerHTML = 'Presentacion lateral del panda';
+// break;
+// default: break; 
 
-
-case imagen6: document.getElementById("primero").src = "imagenes/panda/panda1.jpg"; document.getElementById("segundo").src =
-              "imagenes/panda/panda2.jpg"; document.getElementById("tercero").src = "imagenes/panda/panda3.jpg";
-              document.getElementById("hslide").innerHTML = 'Panda'; document.getElementById("hslide2").innerHTML ='Panda';
-              document.getElementById("hslide3").innerHTML = 'Panda'; document.getElementById("tslide1").innerHTML = 'Presentacion frontal del panda'; document.getElementById("tslide2").innerHTML = 'Presentacion trasera del panda';
-              document.getElementById("tslide3").innerHTML = 'Presentacion lateral del panda';
-break;
-default: break; 
-
-}  
+// }  
  
 }
 
@@ -77,7 +47,7 @@ var imagentext = ""
 for (imagen of imagenes){
 
     imagentext = "imagen"+ i;
-     galeria.innerHTML += `<article id="articulo"> <img id=${imagentext} class= "imagen-galeria"  src= "${imagen}"  alt="img1" onclick="cambioimagen(${imagentext})"  style="border-radius: 50%;"  />  <figcaption id = "texto-imagen">"${titulos[i]}"</figcaption> </a> </article>`;
+     galeria.innerHTML += ` <article class="articulo"> <img id=${imagentext} class= "imagen-galeria"  src= "${imagen}"  alt="img1" onclick="cambioimagen(${i})"    />  <figcaption id = "texto-imagen">"${titulos[i]}"</figcaption> </a> </article>`;
       i++;
      
 
